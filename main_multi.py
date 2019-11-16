17da82601cf7518e86fb95354c216294d172c555
tomoyuki <gemelli.b@gmail.com>
2019-11-16 17:54:14 +0900

    fork

--- main_multi.py
+++ main_multi.py
@@ -17,6 +17,7 @@ import h5py
 
 from model_util import DeepModel, DataSequence
 
+from urllib.request import url2pathname
 
 class ImageSimilarity():
     '''Image similarity.'''
@@ -104,7 +105,9 @@ class ImageSimilarity():
         path, fields = para['path'], para['fields']
         try:
             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
-            res = requests.get(path, headers=headers)
+            requests_session = requests.session()
+            requests_session.mount('file://', LocalFileAdapter())
+            res = requests_session.get(path, headers=headers)
             feature = DeepModel.preprocess_image(BytesIO(res.content))
             return feature, fields
 
@@ -276,6 +279,58 @@ class ImageSimilarity():
 
         return distances
 
+class LocalFileAdapter(requests.adapters.BaseAdapter):
+    """Protocol Adapter to allow Requests to GET file:// URLs
+
+    @todo: Properly handle non-empty hostname portions.
+    """
+
+    @staticmethod
+    def _chkpath(method, path):
+        """Return an HTTP status for the given filesystem path."""
+        if method.lower() in ('put', 'delete'):
+            return 501, "Not Implemented"  # TODO
+        elif method.lower() not in ('get', 'head'):
+            return 405, "Method Not Allowed"
+        elif os.path.isdir(path):
+            return 400, "Path Not A File"
+        elif not os.path.isfile(path):
+            return 404, "File Not Found"
+        elif not os.access(path, os.R_OK):
+            return 403, "Access Denied"
+        else:
+            return 200, "OK"
+
+    def send(self, req, **kwargs):  # pylint: disable=unused-argument
+        """Return the file specified by the given request
+
+        @type req: C{PreparedRequest}
+        @todo: Should I bother filling `response.headers` and processing
+               If-Modified-Since and friends using `os.stat`?
+        """
+        path = os.path.normcase(os.path.normpath(url2pathname(req.path_url)))
+        response = requests.Response()
+
+        response.status_code, response.reason = self._chkpath(req.method, path)
+        if response.status_code == 200 and req.method.lower() != 'head':
+            try:
+                response.raw = open(path, 'rb')
+            except (OSError, IOError) as err:
+                response.status_code = 500
+                response.reason = str(err)
+
+        if isinstance(req.url, bytes):
+            response.url = req.url.decode('utf-8')
+        else:
+            response.url = req.url
+
+        response.request = req
+        response.connection = self
+
+        return response
+
+    def close(self):
+        pass
 
 if __name__ == '__main__':
     similarity = ImageSimilarity()
@@ -296,3 +351,4 @@ if __name__ == '__main__':
     result = similarity.iteration(['test1_id', 'test1_url', 'test2_id', 'test2_url'], thresh=0.845)
     print('Row for source file 1, and column for source file 2.')
     print(result)
+
